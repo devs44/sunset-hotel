45a575a7c8a6566ce869ed03f2746b83e0a95445
Anuska Sthapit <anuskasthapit@sxc.edu.np>
2021-04-28 15:23:43 +0545

    news list page and change in navbar

--- dashboard/views.py
+++ dashboard/views.py
@@ -309,6 +309,7 @@ class RoomSearchView(View):
 class NewsListView(QuerysetMixin, ListView):
     model = News
     template_name = 'dashboard/news/list.html'
+    paginate_by = 5
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -347,15 +348,15 @@ class NewsDeleteView(DeleteMixin, DeleteView):
 
 # newscomments
 
-class NewsCommentTemplateView(TemplateView):
+class NewsCommentTemplateView(ListView):
     model = Comment
     template_name = 'dashboard/news_comment/list.html'
-    form_class = 'NewsForm'
+    context_object_name = 'news'
+    paginate_by = 5
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['news'] = Comment.objects.filter(events__isnull=True)
-        return context
+    def get_queryset(self):
+        return super().get_queryset().filter(events__isnull=True)
+
 
 
 class NewsCommentCreateView(CreateView):
